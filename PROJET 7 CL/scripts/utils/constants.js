b4a0b8bbed32b38a3b38999e660690a61f78a714
AxSF3 <71151267+AxSF3@users.noreply.github.com>
2022-01-09 20:51:15 +0100

    Add files via upload

---
+++ PROJET 7 CL/scripts/utils/constants.js
@@ -0,0 +1,23 @@
+/* DOM elements */
+
+const recipesSection = document.querySelector(".results");
+
+const ingredientChevron = document.querySelector(".ingredient__chevron");
+const ingredientForm = document.querySelector(".ingredient__form");
+const ingredientInput = document.querySelector(".ingredient__input");
+const ingredientWrapper = document.querySelector(".ingredient__results");
+
+const apparatusChevron = document.querySelector(".apparatus__chevron");
+const apparatusForm = document.querySelector(".apparatus__form");
+const apparatusInput = document.querySelector(".apparatus__input");
+const apparatusWrapper = document.querySelector(".apparatus__results");
+
+const ustensilsChevron = document.querySelector(".ustensils__chevron");
+const ustensilsForm = document.querySelector(".ustensils__form");
+const ustensilsInput = document.querySelector(".ustensils__input");
+const ustensilsWrapper = document.querySelector(".ustensils__results");
+
+
+
+
+
